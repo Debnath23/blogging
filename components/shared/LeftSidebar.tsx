1818d359c02fce9bd5d9040703fb9a3d60332ee1
Debnath Mahapatra <128223356+Debnath23@users.noreply.github.com>
2024-06-21 11:40:32 +0530

    sign-out link config

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -2,13 +2,12 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { SignOutButton, SignedIn, useAuth } from "@clerk/nextjs";
 
 import { sidebarLinks } from "@/constants";
 
 const LeftSidebar = () => {
-  const router = useRouter();
   const pathname = usePathname();
 
   const { userId } = useAuth();
@@ -44,8 +43,7 @@ const LeftSidebar = () => {
 
       <div className='mt-10 px-6'>
         <SignedIn>
-          <SignOutButton>
-            <Link href="/sign-in">
+          <SignOutButton redirectUrl="/sign-in">
             <div className='flex cursor-pointer gap-4 p-4'>
               <Image
                 src='/assets/logout.svg'
@@ -56,7 +54,6 @@ const LeftSidebar = () => {
 
               <p className='text-light-2 max-lg:hidden'>Logout</p>
             </div>
-            </Link>
           </SignOutButton>
         </SignedIn>
       </div>
